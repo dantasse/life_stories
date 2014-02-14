6a73e727f67f398b948a0c36c4c8e419d5218989
Dan Tasse <dan.tasse@gmail.com>
2014-02-14 17:39:07 -0500

    ok now it has some boxes where you can tell your story, it's horizontal

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function hideAll() {
 }
 
 var age;
+var lifeBreaks = [];
 
 function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -14,7 +15,7 @@ function isNumber(n) {
 $("#page1done").click(function() {
     var ageStr = $("#age").val();
     age = parseInt(ageStr);
-    if (isNaN(age) || age < 0 || age > 200) { // let's be optimistic here
+    if (isNaN(age) || age < 0 || age > 120) { // let's be optimistic here
         $("#page1error").text("Stop it. How old are you?");
         $("#age").select();
         return;
@@ -29,20 +30,35 @@ function randInt(low, high) {
 }
 
 function setupPage2() {
-    var numLifeBreaks = Math.max(5, age / 25); // you should have at least a few
+    var numLifeBreaks = Math.max(5, age / 7); // you should have at least a few
     // life segments, but no matter how old you are you should have one every
     // few years I guess
-    var lifeBreaks = [];
+    lifeBreaks = [];
     while (lifeBreaks.length < numLifeBreaks && lifeBreaks.length < age-1) {
-        // the -1 is a hack just in case you get someone who's 3 years old;
+        // the age-1 is a hack just in case you get someone who's, say, 3yo;
         // they can only have 2 breaks in their life, at 1 and 2
         newLifeBreak = randInt(1, age);
         if (lifeBreaks.indexOf(newLifeBreak) == -1) {
             lifeBreaks.push(newLifeBreak);
         }
     }
-    lifeBreaks.sort();
+    lifeBreaks.push(0);
+    lifeBreaks.sort(function cmp(a,b) {return a-b;}); // numeric, not lexicographic
 
     $("#page2").show();
-    $("#boxes").text(lifeBreaks);
+    var totalWidth = 900; // meh? 1024 is still popular I guess; borders add to this
+    for (var i = 0; i < lifeBreaks.length; i++) {
+        
+        var newBox = $("<span class='life_chunk'>" + lifeBreaks[i] +
+            "<textarea class='life_chunk_text' rows='5'></textarea></span>");
+        var numYears;
+        if (i == lifeBreaks.length - 1) {
+            numYears = age - lifeBreaks[i];
+        } else {
+            numYears = lifeBreaks[i+1] - lifeBreaks[i];
+        }
+        width = (totalWidth / age) * numYears;
+        newBox.attr({"style": "width:" + width + "px"});
+        $("#boxes").append(newBox);
+    }
 }
