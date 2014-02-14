2b37d267ae6e3465081fbac988e9c3f34a998b19
Dan Tasse <dan.tasse@gmail.com>
2014-02-14 16:31:27 -0500

    make random numbers for divides

--- js/main.js
+++ js/main.js
@@ -20,6 +20,29 @@ $("#page1done").click(function() {
         return;
     }
     hideAll();
-    $("#page2").show();
-    $("#page2").text(age);
+    setupPage2();
 });
+
+// returns a random integer between low (inclusive) and high (exclusive)
+function randInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function setupPage2() {
+    var numLifeBreaks = Math.max(5, age / 25); // you should have at least a few
+    // life segments, but no matter how old you are you should have one every
+    // few years I guess
+    var lifeBreaks = [];
+    while (lifeBreaks.length < numLifeBreaks && lifeBreaks.length < age-1) {
+        // the -1 is a hack just in case you get someone who's 3 years old;
+        // they can only have 2 breaks in their life, at 1 and 2
+        newLifeBreak = randInt(1, age);
+        if (lifeBreaks.indexOf(newLifeBreak) == -1) {
+            lifeBreaks.push(newLifeBreak);
+        }
+    }
+    lifeBreaks.sort();
+
+    $("#page2").show();
+    $("#boxes").text(lifeBreaks);
+}
