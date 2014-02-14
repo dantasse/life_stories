635063d3916b1abd8e1f99baa28c425aa193ad96
Dan Tasse <dan.tasse@gmail.com>
2014-02-14 15:29:35 -0500

    make it ask for your age

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 
-var hideAll = function() {
+function hideAll() {
     $("#page1").hide();
     $("#page2").hide();
     $("#page3").hide();
 }
 
+var age;
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 $("#page1done").click(function() {
+    var ageStr = $("#age").val();
+    age = parseInt(ageStr);
+    if (isNaN(age) || age < 0 || age > 200) { // let's be optimistic here
+        $("#page1error").text("Stop it. How old are you?");
+        $("#age").select();
+        return;
+    }
     hideAll();
     $("#page2").show();
+    $("#page2").text(age);
 });
