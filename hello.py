b1299f4e00701a70568eef2ff6f0cc33f4060a6f
Dan Tasse <dan.tasse@gmail.com>
2014-02-17 17:15:02 -0500

    trying to add mongohq

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask, render_template
+import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(os.environ['MONGOHQ_URL'])
+mongo_db = client.get_default_database()
+
+# print os.environ['PYTHONPATH']
+print os.environ.get('MONGOHQ_URL')
+# db = pymongo.Connection('
 @app.route('/')
 def hello():
     return render_template('index.html')
